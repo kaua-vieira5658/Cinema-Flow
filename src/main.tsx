88d5cc9225a124f1d44debebc5d5b3d1dff2eb7a
kaua-vieira5658 <kauavieira5658@gmail.com>
2025-04-27 23:54:43 -0300

    Criando as primeira funcionalidades

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import SearchModalContext from "./context/SearchModalContext.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// Renderizando o aplicativo
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <SearchModalContext>
+      <App />
+    </SearchModalContext>
+  </StrictMode>
+);
